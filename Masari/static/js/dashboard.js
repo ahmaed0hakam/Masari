3fc30af8b17744a6eb7e2ec65eec9951dc7e5562
Ahmad Alhafi <a.alhafi@classera.com>
2024-04-25 17:23:02 +0300

    dashboard FE ready

---
+++ Masari/static/js/dashboard.js
@@ -0,0 +1,80 @@
+$(document).ready(function() {
+    const inputField = $('input');
+    const learningPathButton = $('.buttons button#pathButton');
+    const courseButton = $('.buttons button#courseButton');
+
+    // Disable buttons initially
+    learningPathButton.prop('disabled', true);
+    courseButton.prop('disabled', true);
+
+    inputField.on('input', function() {
+        const inputValueLength = inputField.val().length;
+        learningPathButton.prop('disabled', inputValueLength < 3);
+        courseButton.prop('disabled', inputValueLength < 3);
+    });
+
+    // Handle button click events
+    learningPathButton.on('click', function() {
+        generateLearningPath();
+    });
+
+    courseButton.on('click', function() {
+        generateCourse();
+    });
+});
+
+function generateLearningPath() {
+    $.ajax({
+        url: '/api/generate_learningpath',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function(data) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Learning path generated successfully!',
+                showConfirmButton: false,
+                timer: 1500
+            }).then(() => {
+                const pathId = data.id;
+                window.location.href = `/learningpath?id=${pathId}`;
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error('Error generating the learning path:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Failed to generate the learning path',
+                text: 'Please try again later'
+            });
+        }
+    });
+}
+
+function generateCourse() {
+    $.ajax({
+        url: '/api/generate_courses',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({}),
+        success: function(data) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Course generated successfully!',
+                showConfirmButton: false,
+                timer: 1500
+            }).then(() => {
+                const courseId = data.id;
+                window.location.href = `/course?id=${courseId}`;
+            });
+        },
+        error: function(xhr, status, error) {
+            console.error('Error generating the course:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Failed to generate the course',
+                text: 'Please try again later'
+            });
+        }
+    });
+}
